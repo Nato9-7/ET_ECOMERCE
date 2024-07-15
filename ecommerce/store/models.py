62949b02b87a5ce276089e3562a24ff9da5975a9
Nato9-7 <re.diazs@duocuc.cl>
2024-07-14 20:34:16 -0400

    no sé que hice

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -11,7 +11,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
 
     def __str__(self):
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 from django.db import connection
 
 def home(request):
-    products = Product.objects.all()
-    return render(request, 'index.html', {'products': products})
+    toys_category = Category.objects.get(name='juguete')
+    meats_category = Category.objects.get(name='carne')
+    bebida_category = Category.objects.get(name='bebida')
+    lacteo_category = Category.objects.get(name='lacteo')
+    snak_category = Category.objects.get(name='snak')
+    # Filtrar productos por categoría
+    toys_products = Product.objects.filter(category = toys_category)
+    meats_products = Product.objects.filter(category = meats_category)
+    bebida_products = Product.objects.filter(category = bebida_category)
+    lacteo_products = Product.objects.filter(category = lacteo_category)
+    snak_products = Product.objects.filter(category = snak_category)
+    return render(request, 'index.html', {
+        'toys_products': toys_products,
+        'meats_products': meats_products,
+        'lacteo_products': lacteo_products,
+        'snak_products': snak_products,
+        'bebida_products': bebida_products
+    })
 
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'product_list.html', {'products': products})
 
 def raw_product_list(request):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM store_product")
-        products = cursor.fetchall()
-    return render(request, 'raw_product_list.html', {'products': products})
+    productos =  Product.objects.filter(category = 3)
+    return render(request, "raw_product_list.html", {"productos" : productos})
 
